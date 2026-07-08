c32569f8545b670da62590ff9d170283f4c0e9ce
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2026-07-08 17:38:16 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    fonts: [{
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+    },
+    {
+        provider: fontProviders.google(),
+        name: 'Oswald',
+        cssVariable: '--font-oswald',
+    },
+    {
+        provider: fontProviders.google(),
+        name: 'Bebas Neue',
+        cssVariable: '--font-bebas-neue',
+    },
+],
+});
