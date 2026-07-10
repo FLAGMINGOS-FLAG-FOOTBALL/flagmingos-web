170333f50e075a8d172dd0f14ffe0f44ae816a1d
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2026-07-10 17:24:50 +0200

    feat: Add new adhesion components and enhance existing ones for improved user experience

---
+++ src/data/adhesion.ts
@@ -0,0 +1,129 @@
+export const sections = [
+  {
+    tag: 'JEUNES',
+    title: 'École de flag',
+    highlight: 'Former les futurs joueurs',
+    description:
+      "Apprendre les bases du flag football, progresser ensemble et découvrir l'esprit d'équipe.",
+    price: 100,
+    public: 'U7 à U18',
+    details: [
+        "Développement des compétences techniques et tactiques",
+        "Apprentissage du jeu collectif",
+    ],
+    trainings: [
+      {
+        group: 'U15 - U18',
+        sessions: [
+          {
+            day: 'Mardi',
+            time: '18h30 - 20h00',
+          },
+          {
+            day: 'Dimanche',
+            time: '11h00 - 12h00',
+          },
+        ],
+      },
+      {
+        group: 'U7 - U13',
+        sessions: [
+          {
+            day: 'Jeudi',
+            time: '18h30 - 20h00',
+          },
+          {
+            day: 'Dimanche',
+            time: '10h00 - 11h00',
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    tag: 'LOISIR',
+    title: 'Section Loisir',
+    highlight: 'Jouer sans pression',
+    description:
+      "Découvrir le flag football, progresser à son rythme et partager une expérience sportive conviviale.",
+    price: 100,
+    public: 'Dès 17 ans',
+    details: [
+        "Accès à tout les entraînements",
+        "Pas de compétition, juste le plaisir de jouer",
+    ],
+    trainings: [
+      {
+        group: 'Mixte',
+        sessions: [
+          {
+            day: 'Mardi',
+            time: '20h00 - 22h00',
+          },
+          {
+            day: 'Jeudi',
+            time: '20h00 - 22h00',
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    tag: 'COMPÉTITION',
+    title: 'Section Compétition',
+    highlight: 'Représenter l’équipe',
+    description:
+      "Rejoins nos équipes engagées en championnat régional et national avec une préparation sportive et tactique renforcée.",
+    price: 180,
+    femalePrice: 130,
+    public: 'Dès 17 ans',
+    details: [
+        "Accès à l'ensemble des compétitions régionales et nationales"
+    ],
+    trainings: [
+      {
+        group: 'Mixte',
+        sessions: [
+          {
+            day: 'Mardi',
+            time: '20h00 - 22h00',
+          },
+          {
+            day: 'Jeudi',
+            time: '20h00 - 22h00',
+          },
+        ],
+      },
+      {
+        group: 'Coupe de France Féminine',
+        sessions: [
+          {
+            day: 'Mardi',
+            time: '19h00 - 20h00',
+          },
+        ],
+      },
+      {
+        group: 'Coupe de France Masculine',
+        sessions: [
+          {
+            day: 'Jeudi',
+            time: '19h00 - 20h00',
+          },
+        ],
+      },
+    ],
+  },
+]
+
+export const HELLOASSO_URL = 'https://www.helloasso.com/associations/flagmingos-flag-football/adhesions/adhesion-2026-2027'
+
+export const gear = [
+    "1 tee-shirt, sweat...",
+    "1 short, bas de jogging, ou legging sans poche",
+    "1 paire de crampons moulés (crampons métalliques et bouts anti-usure interdits)",
+    "1 gourde ou bouteille d’eau",
+    "Recommandé: un protège-dents",
+]
