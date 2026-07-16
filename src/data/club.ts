ec86bd6eea7f1cadb39bff16b9e93d0ae2f1b803
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2026-07-16 12:52:25 +0200

    Add TradingCard display for staff in club page

---
+++ src/data/club.ts
@@ -0,0 +1,113 @@
+export const board = [
+  {
+    role: 'Président',
+    name: 'Nicolas Leveau',
+    category: 'bureau',
+    number: 8,
+  },
+  {
+    role: 'Secrétaire',
+    name: 'Jean-Bernard Bille',
+    category: 'bureau',
+    number: 14,
+  },
+  {
+    role: 'Trésorière',
+    name: 'Camille Bernard',
+    category: 'bureau',
+    number: 15,
+  },
+  {
+    role: 'Head Coach Séniors',
+    name: 'Sylvain Vallot',
+    category: 'ca',
+    number: 21
+  },
+  {
+    role: 'Head Coach Jeunes',
+    name: 'Nuria Inglada',
+    category: 'ca',
+    number: 1
+  },
+  {
+    role: 'Responsable Arbitrage',
+    name: 'Flavien Normandin',
+    category: 'ca',
+    number: 3
+  },
+  {
+    role: 'Responsable Communication & Partenariats',
+    name: 'Charley Bui',
+    category: 'ca',
+    number: 99
+  },
+  {
+    role: 'Responsable Événementiel',
+    name: 'Nom Prenom',
+    category: 'ca',
+    number: 0
+  },
+  {
+    role: 'Représentant des joueurs',
+    name: 'Nom Prenom',
+    category: 'ca',
+    number: 0
+  },
+  {
+    role: 'Représentant des joueuses',
+    name: 'Nom Prenom',
+    category: 'ca',
+    number: 0
+  },
+]
+
+export const coachs = [
+  {
+    role: 'Head Coach Séniors',
+    name: 'Sylvain Vallot',
+    category: 'headcoach',
+  },
+  {
+    role: 'Head Coach Jeunes',
+    name: 'Nuria Inglada',
+    category: 'headcoach',
+  },
+  {
+    role: 'Coachs Séniors',
+    name: 'Manon Pialat',
+    category: 'coach',
+  },
+  {
+    role: 'Coachs Jeunes',
+    name: 'Simon Barrois',
+    category: 'coach',
+  },
+  {
+    role: 'Coachs Jeunes',
+    name: 'Nicolas Rateau',
+    category: 'coach',
+  },
+  {
+    role: 'Préparation physique',
+    name: 'Rémy Gaudry',
+    category: 'coach',
+  },
+  {
+    role: 'Préparation physique',
+    name: 'Nuria Inglada Soler',
+    category: 'coach',
+  }
+]
+
+export const medical = [
+  {
+    role: 'Ostéopathe',
+    name: 'Thomas Mouries',
+    category: 'coach',
+  },
+  {
+    role: 'Kinésithérapeute',
+    name: 'Rémy Gaudry',
+    category: 'coach',
+  },
+]
