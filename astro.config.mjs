9288851da1321505caba297115f7c0fbc22458f6
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2026-07-20 17:32:42 +0200

    feat: implement Netlify form in new netlify-forms page and remove from Booking component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://flagmingos.fr',
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
   },
 
   adapter: netlify(),
-  integrations: [sitemap()],
-});
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('netlify-forms'),
+    }),
+  ],
+})
